4210e17bc5b13afd60487b84b606d0102e21fae0
elboqueronpaco <elboqueronpaco@gmail.com>
2022-10-16 19:39:44 +0200

    setup

---
+++ apps/design-system/src/stories/pages/index.stories.tsx
@@ -0,0 +1,8 @@
+import Home from '../../pages'
+
+export default {
+    tirle: 'Pages/Home',
+    component: Home
+}
+
+export const HomePage = () => <Home />
---
+++ apps/mooc/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const index: NextPage = () => {
+  return (
+    <div>Pagina de inicio Mooc</div>
+  )
+}
+
+export default index
